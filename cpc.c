b766a6b610d222ba70df12c69ad1878e50c424dd
archeryue <archeryue7@gmail.com>
2021-07-01 16:57:39 +0800

    ADD: memory & register

--- cpc.c
+++ cpc.c
@@ -1,13 +1,24 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
 #include <string.h>
 
-#define int64 long long
-
-int64 MAX_SIZE = 256 * 1024;
-int64 token;
+uint32_t MAX_SIZE = 256 * 1024 * 4; // 1MB = 256k*32bit
+uint32_t token;
 char* src;
+
+uint32_t* code, 		// code segment
+		* code_dump, 	// for dump
+		* stack;    	// stack
+char* data;       		// data segment
+
+uint32_t* pc,
+		* sp,
+		* bp;
+
+uint32_t ax, 	// common register
+		 cycle;
 
 void tokenizer() {
 	// todo
@@ -19,6 +30,32 @@ void parser() {
 
 void expEval() {
 	// todo
+}
+
+int initVM() {
+	// allocate memory for virtual machine
+    if (!(code = code_dump = malloc(MAX_SIZE))) {
+        printf("could not malloc(%d) for code segment\n", MAX_SIZE);
+		return -1;
+    }
+    if (!(data = malloc(MAX_SIZE))) {
+        printf("could not malloc(%d) for data segment\n", MAX_SIZE);
+		return -1;
+    }
+    if (!(stack = malloc(MAX_SIZE))) {
+        printf("could not malloc(%d) for stack segment\n", MAX_SIZE);
+		return -1;
+    }
+
+    memset(code, 0, MAX_SIZE);
+    memset(data, 0, MAX_SIZE);
+    memset(stack, 0, MAX_SIZE);
+
+	// init register
+	bp = sp = stack + MAX_SIZE; // stack downwards
+	ax = 0;
+
+	return 0;
 }
 
 int runVM() {
@@ -34,7 +71,7 @@ int main(int argc, char** argv) {
     }
 
     if (!(src = malloc(MAX_SIZE))) {
-        printf("could not malloc(%lld) for source area\n", MAX_SIZE);
+        printf("could not malloc(%d) for source area\n", MAX_SIZE);
         return -1;
     }
 
@@ -44,6 +81,10 @@ int main(int argc, char** argv) {
 	src[cnt] = '\0';
 	fclose(fp);
 
+	// init memory & register
+	if (initVM() != 0) return -1;
+	// parser: tokenize & parse
 	parser();
+
     return runVM();
 }
