8b8f05ec8920f184be1cb367c268fd137fb68929
archeryue <archeryue7@gmail.com>
2021-07-02 11:53:10 +0800

    FIX: some bug & define int32 int64

--- cpc.c
+++ cpc.c
@@ -7,20 +7,22 @@
 #include <string.h>
 
 #define ok 0
+#define int int32_t
+#define int64 int64_t
 
-int64_t MAX_SIZE = 128 * 1024 * 4; // 1MB = 128k * 64bit
+int64 MAX_SIZE = 128 * 1024 * 8; // 1MB = 128k * 64bit
 
-int64_t * code,         // code segment
-        * code_dump,    // for dump
-        * stack;        // stack segment
-char * data;            // data segment
+int64 * code,         // code segment
+      * code_dump,    // for dump
+      * stack;        // stack segment
+char  * data;         // data segment
 
-int64_t * pc,           // pc register
-        * sp,           // esp register
-        * bp;           // ebp register
+int64 * pc,           // pc register
+      * sp,           // esp register
+      * bp;           // ebp register
 
-int64_t ax,             // common register
-        cycle;
+int64 ax,             // common register
+      cycle;
 
 // instruction set: copy from c4, change ENT/ADJ/LEV to NSF/CSF/RET, add CALL.
 enum {IMM, LEA, JMP, JZ, JNZ, CALL, NSF, CSF, RET, LI, LC, SI, SC, PUSH,
@@ -44,12 +46,12 @@ char * src,
      * src_dump;
 
 // symbol table & reuse pointer
-int64_t * symbol_table,
-        * symbol_ptr,
-        * main_ptr;
+int64 * symbol_table,
+      * symbol_ptr,
+      * main_ptr;
 
-int64_t token, token_val;
-int64_t line;
+int64 token, token_val;
+int64 line;
 
 void tokenize() {
     char* ch_ptr;
@@ -77,7 +79,7 @@ void tokenize() {
                 symbol_ptr += PtrSize;
             }
             // add new symbol
-            symbol_ptr[Name] = (int64_t)ch_ptr;
+            symbol_ptr[Name] = (int64)ch_ptr;
             symbol_ptr[Hash] = token;
             token = symbol_ptr[Token] = Id;
             return;
@@ -89,8 +91,8 @@ void tokenize() {
                 while (*src >= '0' && *src <= '9') token_val = token_val * 10 + *src++ - '0';
             //HEX, ch_ptr with 0x
             else if (*src == 'x' || *src == 'X')
-                while ((token = *++src) && (token >= '0' && token <= '9') || (token >= 'a' && token <= 'f')
-                        || (token >= 'A' && token <= 'F'))
+                while ((token = *++src) && ((token >= '0' && token <= '9') || (token >= 'a' && token <= 'f')
+                        || (token >= 'A' && token <= 'F')))
                     // COOL!
                     token_val = token_val * 16 + (token & 0xF) + (token >= 'A' ? 9 : 0);
             // OCT, start with 0
@@ -110,7 +112,7 @@ void tokenize() {
                 if (token == '"') *data++ = token_val;
             }
             src++;
-            if (token == '"') token_val = (int64_t)ch_ptr; 
+            if (token == '"') token_val = (int64)ch_ptr; 
             // single char is Num
             else token = Num;
             return;
@@ -157,9 +159,9 @@ void exprEval() {
 }
 
 void prepKeyword() {
-    char* keyword = "char int int64_t enum if else return sizeof while "
+    char* keyword = "char int int64 enum if else return sizeof while "
         "open read close printf malloc free memset memcmp exit void main";
-    int64_t i;
+    int64 i;
     // add keywords to symbol table
     i = Char; while (i <= While) {tokenize(); symbol_ptr[Token] = i++;}
     // add Native CALL to symbol table
@@ -197,22 +199,22 @@ int initVM() {
 }
 
 int runVM() {
-    int64_t op;
-    int64_t* tmp;
+    int64 op;
+    int64* tmp;
     while (1) {
         op = *pc++; // read instruction
         // load & save
         if (op == IMM)          ax = *pc++;                         // load immediate(or global addr)
-        else if (op == LEA)     ax = (int64_t)bp + *pc++;           // load local addr
+        else if (op == LEA)     ax = (int64)bp + *pc++;           // load local addr
         else if (op == LC)      ax = *(char*)ax;                    // load char
-        else if (op == LI)      ax = *(int64_t*)ax;                 // load int
+        else if (op == LI)      ax = *(int64*)ax;                 // load int
         else if (op == SC)      *(char*)*sp++ = ax;                 // save char to stack
-        else if (op == SI)      *(int64_t*)*sp++ = ax;              // save int to stack
+        else if (op == SI)      *(int64*)*sp++ = ax;              // save int to stack
         else if (op == PUSH)    *--sp = ax;                         // push ax to stack
         // jump
-        else if (op == JMP)     pc = (int64_t*)*pc;                 // jump
-        else if (op == JZ)      pc = ax ? pc + 1 : (int64_t*)*pc;   // jump if ax == 0
-        else if (op == JNZ)     pc = ax ? (int64_t*)*pc : pc + 1;   // jump if ax != 0
+        else if (op == JMP)     pc = (int64*)*pc;                 // jump
+        else if (op == JZ)      pc = ax ? pc + 1 : (int64*)*pc;   // jump if ax == 0
+        else if (op == JNZ)     pc = ax ? (int64*)*pc : pc + 1;   // jump if ax != 0
         // arithmetic
         else if (op == OR)      ax = *sp++ |  ax;
         else if (op == XOR)     ax = *sp++ ^  ax;
@@ -232,13 +234,13 @@ int runVM() {
         else if (op == MOD)     ax = *sp++ %  ax;
         // some complicate instructions for function call
         // call function: push pc + 1 to stack & pc jump to func addr(pc point to)
-        else if (op == CALL)    {*--sp = (int64_t)(pc+1); pc = (int64_t*)*pc;}
+        else if (op == CALL)    {*--sp = (int64)(pc+1); pc = (int64*)*pc;}
         // new stack frame: save bp, bp -> caller stack, stack add frame
-        else if (op == NSF)     {*--sp = (int64_t)bp; bp = sp; sp = sp - *pc++;}
+        else if (op == NSF)     {*--sp = (int64)bp; bp = sp; sp = sp - *pc++;}
         // clean stack frame: stack clean frame, same as x86 : add esp, <size>
         else if (op == CSF)     sp = sp + *pc++;
         // return caller: retore stack, retore old bp, pc point to caller code addr(store by CALL) 
-        else if (op == RET)     {sp = bp; bp = (int64_t*)*sp++; pc = (int64_t*)*sp++;}        
+        else if (op == RET)     {sp = bp; bp = (int64*)*sp++; pc = (int64*)*sp++;}        
         // end for call function.
         // native call
         else if (op == OPEN)    {ax = open((char*)sp[1], sp[0]);}
@@ -246,9 +248,9 @@ int runVM() {
         else if (op == READ)    {ax = read(sp[2], (char*)sp[1], *sp);}
         else if (op == PRTF)    {tmp = sp + pc[1]; ax = printf((char*)tmp[-1], tmp[-2], tmp[-3],
                                                                     tmp[-4], tmp[-5], tmp[-6]);}
-        else if (op == MALC)    {ax = (int64_t)malloc(*sp);}
+        else if (op == MALC)    {ax = (int64)malloc(*sp);}
         else if (op == FREE)    {free((void*)*sp);}
-        else if (op == MSET)    {ax = (int64_t)memset((char*)sp[2], sp[1], *sp);}
+        else if (op == MSET)    {ax = (int64)memset((char*)sp[2], sp[1], *sp);}
         else if (op == MCMP)    {ax = memcmp((char*)sp[2], (char*)sp[1], *sp);}
         else if (op == EXIT)    {printf("exit(%lld)\n", *sp); return *sp;}
         else {
@@ -260,7 +262,7 @@ int runVM() {
 }
 
 int loadCode(char* file) {
-    int64_t fd;
+    int64 fd;
     // use open/read/close for bootstrap.
     if ((fd = open(file, 0)) < 0) {
         printf("could not open source code(%s)\n", file);
@@ -271,7 +273,7 @@ int loadCode(char* file) {
         return -1;
     }
     line = 0;
-    int64_t cnt;
+    int64 cnt;
     if ((cnt = read(fd, src, MAX_SIZE - 1)) <= 0) {
         printf("could not read source code(%lld)\n", cnt);
         return -1;
